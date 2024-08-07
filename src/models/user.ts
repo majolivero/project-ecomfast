52405c9bf06d4f7bf2254f5ed54ad04bed089c47
majolivero <majolivero28@gmail.com>
2024-08-07 17:44:49 -0500

    creación de modelo user

---
+++ src/models/user.ts
@@ -0,0 +1,46 @@
+import {
+    Table,
+    Column,
+    Model,
+    DataType,
+    PrimaryKey,
+    AutoIncrement,
+    BelongsTo,
+    ForeignKey,
+}from 'sequelize-typescript';
+import { Role } from './role';
+
+@Table({
+    tableName : "users",
+    timestamps : true,
+})
+export class User extends Model {
+    @PrimaryKey
+    @AutoIncrement
+    @Column({
+        type: DataType.INTEGER
+    })
+    id! : number;
+
+    @Column({
+        type : DataType.STRING,
+        allowNull : false,
+    })
+    email! : string;
+
+    @Column({
+        type : DataType.STRING,
+        allowNull : false,
+    })
+    password! : string;
+
+    @ForeignKey(() => Role)
+    @Column({
+        type: DataType.STRING,
+        allowNull: false,
+    })
+    roleId! : number;
+
+    @BelongsTo(() => Role)
+    role! : Role;
+}
