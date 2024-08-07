dcb2b5acbf84a76363c6505f3dc68e6af89c8e8b
majolivero <majolivero28@gmail.com>
2024-08-07 17:11:05 -0500

    creación del db.ts

---
+++ src/config/db.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from 'sequelize-typescript';
+import { Product,User,Role,Cart,ProductCart,Order,Entities,Permissions} from '../models';
+
+const sequelize: Sequelize = new Sequelize({
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: '',
+    database: 'projectEcomfast',
+    models: [Product,User,Role,Cart,ProductCart,Order,Entities,Permissions],
+});
+
+export default sequelize;
