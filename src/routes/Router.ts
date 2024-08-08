d2dc61b6f3f4377f87dd59083434e424c550e9a3
majolivero <majolivero28@gmail.com>
2024-08-07 21:33:30 -0500

    creación de archivo Router.ts

---
+++ src/routes/Router.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { orderRouter,productRouter,userRouter,cartRouter} from './';
+
+const router = Router();
+
+router.use('/user',userRouter);
+router.use('/product',productRouter);
+router.use('/order',orderRouter);
+router.use('cartRouter',cartRouter);
+
+export default router;
